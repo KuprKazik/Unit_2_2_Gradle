df5b0b7ab923038330a0176c5952741ab8504103
KuprKazik <kuprkazik94@gmail.com>
2024-12-07 18:01:47 +0300

    test failed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then().log().all()
                 .statusCode(200)
-                .body("data", org.hamcrest.Matchers.equalTo("Hello, world"));
+                .body("data", org.hamcrest.Matchers.equalTo("Hello"));
 
     }
 }
